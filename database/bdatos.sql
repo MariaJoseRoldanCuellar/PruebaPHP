132d5d0ce746f506dec916599582e03760d0be3a
Maria Jose Roldan Cuellar <roldancuellarmariajose@gmail.com>
2025-09-01 19:55:20 -0500

    relacion base de datos

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -37,3 +37,26 @@ create table if not exists t_producto(
     constraint pk_producto   primary key(id_producto),
     constraint fk_producto_categoria foreign key(id_categoria)references t_categoria(id_categoria)
 )engine=InnoDB;
+
+create table if not exists t_pedido(
+    id_pedido        int(11)      auto_increment      not null,
+    id_usuario       int(11)                          not null,
+    fecha            date                             not null,
+    hora             time                             not null,
+    ciudad           varchar(100)                     not null,
+    direccion        varchar(200)                     not null,
+    costo            float(10,2)                      not null,
+    estado           varchar(20)                      not null,
+    constraint pk_pedido   primary key(id_pedido),
+    constraint fk_pedido_usuario foreign key(id_usuario)references t_usuario(id_usuario)
+)engine=InnoDB;
+
+create table if not exists t_detalle_pedido(
+    id_detalle_pedido   int(11)     auto_increment    not null,
+    id_pedido           int(11)                       not null,
+    id_producto         int(11)                       not null,
+    cantidad            int(11)                       not null,
+   constraint pk_detalle_pedido   primary key(id_detalle_pedido),
+    constraint fk_pedido foreign key(id_pedido)references t_pedido(id_pedido),
+    constraint fk_producto foreign key(id_producto)references t_producto(id_producto)
+)engine=InnoDB;
