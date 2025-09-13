983dcca2f471dbaa5ad6e13b01585a6dd7ac0d71
Maria Jose Roldan Cuellar <roldancuellarmariajose@gmail.com>
2025-09-12 20:38:59 -0500

    creacion mvc y getters con setters

---
+++ models/UsuarioModel.php
@@ -0,0 +1,71 @@
+<?php
+require_once 'config/database.php';
+
+class Usaurio{
+    public $db;
+    public function __construct(){
+        $this->db= database::conectar();
+    }
+
+
+    public $id_usuario;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $rol;
+    public $imagen;
+
+
+  function getID_usuario()
+    {
+
+    return $this->id_usuario;
+    }
+
+    function getNombre(){
+    return $this->nombre;
+   }
+
+    function getApellido(){
+    return $this->apellido;
+    }
+
+    function getEmail(){
+    return $this->email;
+    }
+
+    function getPassword(){
+    return $this->password;
+    }
+
+    function getRol(){
+    return $this->rol;
+    }
+
+    function getImagen(){
+    return $this->imagen;
+   }
+   function setID_usuario($id_usuario){
+    $this->id_usuario=$id_usuario;
+   }
+   function setNombre($nombre){
+    $this->nombre=$nombre;
+   }
+   function setApellido($apellido){
+    $this->apellido=$apellido;
+   }
+   function setEmail($email){
+    $this->email=$email;
+   }
+   function setPassword($password){
+    $this->password=$password;
+   }
+   function setRol($rol){
+    $this->rol=$rol;
+   }
+   function setImagen($imagen){
+    $this->imagen=$imagen;
+   }
+}
+
